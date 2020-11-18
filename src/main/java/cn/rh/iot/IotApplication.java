ab0e1170197857c1dadc19b76726623d729cfaae
nmgyaoyuan <3552822@qq.com>
2020-11-18 10:22:11 +0800

    微调IotApplication

--- src/main/java/cn/rh/iot/IotApplication.java
+++ src/main/java/cn/rh/iot/IotApplication.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 
 @Slf4j
-public class IotApplication extends Thread{
+public class IotApplication{
 
     public static void main(String[] args) {
 
